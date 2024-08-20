e91672302cf973633699fdea19cec2d35a1709ec
Agustin-Ibarra <agustingsoc@gmail.com>
2024-08-20 12:29:45 -0300

    w

--- source/controllers/routes_controllers.ts
+++ source/controllers/routes_controllers.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { Router } from 'express';
-import { paymentForItems, subscriptionPayments } from './routes/payments';
+import { paymentForItems, subscriptionPayments } from '../routes/payments';
 import { checkItems, checkSubscriptionItem } from '../middlewares/middlewares/middleware';
 
 const router = Router();
